05e4b8817b8dcf276a9a33bedcf0734928155067
Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
2023-05-09 16:42:13 +0300

    Signed-off-by: Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
    	modified:   src/components/vkForm/VKForm.jsx
    	modified:   src/components/vkForm/VKForm.module.css

--- src/components/vkForm/VKForm.jsx
+++ src/components/vkForm/VKForm.jsx
@@ -7,8 +7,43 @@ const VKForm = () => {
         tower: '',
         floor: '',
         date: '',
+        beg_time: '',
+        end_time: '',
         comment: '',
     })
+    const [error, setError] = useState('')
+
+    const getError = () => {
+        if(reservation.tower === '' || reservation.floor === '' || reservation.date === '' || reservation.beg_time === '' || reservation.end_time === '')
+            return 'Заполните поля'
+        if(reservation.beg_time > reservation.end_time)
+            return 'Время указано некорректно'
+        return ''     
+    }
+
+    const send = () => {
+        let error = getError();
+        if(error === '')
+        {
+            console.log(JSON.stringify(reservation))
+            setError('')
+        }
+        else 
+            setError(error)
+    }
+
+    const clear = () => {
+        const emptyReservation = {
+            tower: '',
+            floor: '',
+            date: '',
+            beg_time: '',
+            end_time: '',
+            comment: '',
+        }
+
+        setReservation(emptyReservation)
+    }
 
 
   return (
@@ -29,21 +64,22 @@ const VKForm = () => {
             }
         </select>
 
-        <input className={classes.input} type="date"/>
+        <input className={classes.input} type="date" value={reservation.date} onChange={event => setReservation({...reservation, date: event.target.value})}/>
         <div className={classes.time}>
             <span>Время начала переговоров</span>
-            <input className={classes.input} type="time"/>
+            <input className={classes.input} type="time" value={reservation.beg_time} onChange={event => setReservation({...reservation, beg_time: event.target.value})}/>
             <span>Время конца переговоров</span>
-            <input className={classes.input} type="time"/>
+            <input className={classes.input} type="time" value={reservation.end_time} onChange={event => setReservation({...reservation, end_time: event.target.value})}/>
         </div>
 
-        <textarea className={classes.comment} rows="5" placeholder='Дополнительная информация'></textarea>
+        <textarea className={classes.comment} rows="5" placeholder='Дополнительная информация' value={reservation.comment} onChange={event => setReservation({...reservation, comment: event.target.value})}></textarea>
 
         <div className={classes.controlPanel}>
-            <button className={classes.btn}>Отправить</button>
-            <button className={classes.btn}>Очистить</button>
+            <button className={classes.btn} onClick={() => send()}>Отправить</button>
+            <button className={classes.btn} onClick={() => clear()}>Очистить</button>
         </div>
 
+        <span className={classes.error}>{error}</span>
     </div>
   )
 }
